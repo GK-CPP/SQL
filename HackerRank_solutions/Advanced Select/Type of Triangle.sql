8d879d9f823140f080a2512889824356c803cc51
Gulam Kibria Chowdhury <43819400+GK-CPP@users.noreply.github.com>
2023-08-22 17:41:06 +0600

    advanced Select change

--- HackerRank_solutions/Advanced Select/Type of Triangle.sql
+++ HackerRank_solutions/Advanced Select/Type of Triangle.sql
@@ -1,8 +1,8 @@
 select 
-case
-    when a+b<=c or b+c<=a or a+c<=b  then 'Not A Triangle'
-    when a=b and a=c then 'Equilateral'
-    when a=b or b=c or a=c then 'Isosceles'
-    else 'Scalene'
-end
+    case
+        when a+b<=c or b+c<=a or a+c<=b  then 'Not A Triangle'
+        when a=b and a=c then 'Equilateral'
+        when a=b or b=c or a=c then 'Isosceles'
+        else 'Scalene'
+    end
 from triangles;
